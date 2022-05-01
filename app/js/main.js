e4f6c941567f9f6f4609224a2c35931171370ea6
Niko <niko.nikov2001@gmail.com>
2022-05-01 22:05:17 +0400

    add all

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.top-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: false,
+    });
+
+
+
+
+    var mixer = mixitup('.week__content');
+    var mixer = mixitup('.design__content');
+});
